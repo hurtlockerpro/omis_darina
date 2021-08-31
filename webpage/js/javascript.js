d4e45c81e8f60f06fc5d0f3db4fb0ab6c7e96a95
vahermihkel <vahermihkel@gmail.com>
2021-08-31 21:06:03 +0300

    js v1

---
+++ webpage/js/javascript.js
@@ -0,0 +1,14 @@
+
+console.log('Hello world!') 
+
+let a = 'txt'
+
+//console.log()
+
+let z = true ? '1' : '2'
+
+for (let index = 0; index < 5; index++) {
+    const element = index; 
+    console.log(element)
+}
+
